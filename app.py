d320c98b901bb53c11084d70ae2ac0e71ad3818e
JBayona <jordavids_22@hotmail.com>
2021-05-29 13:57:52 -0500

    Endpoint with routes.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import collections
 from flask import Flask, json, render_template, make_response, jsonify, request
 
 app = Flask(__name__)
@@ -48,6 +49,20 @@ def query_string():
 @app.route("/json")
 def get_json():
   res = make_response(jsonify(INFO), 200)
+  return res
+
+@app.route("/json/<collection>/<member>")
+def get_data(collection, member):
+  if collection in INFO:
+    member = INFO[collection].get(member)
+    if member:
+      res = make_response(jsonify({"res": member}), 200)
+      return res
+    
+    res = make_response(jsonify({"error": "Member not found"}), 400)
+    return res
+  
+  res = make_response(jsonify({"error": "Collection not found"}), 400)
   return res
 
 if __name__ == "__main__":
