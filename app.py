6569f6747500adefac3d58b9a312a59a9b811860
JBayona <jordavids_22@hotmail.com>
2021-05-29 14:16:52 -0500

    Delete endpoint.

--- app.py
+++ app.py
@@ -93,6 +93,17 @@ def update_collection(collection, member):
   res = make_response(jsonify({"error": "Collection not found"}), 400)
   return res
 
+# DELETE METHOD
+@app.route("/json/<collection>", methods=["DELETE"])
+def delete_collection(collection):
+  if collection in INFO:
+    del INFO[collection]
+    res = make_response(jsonify(INFO), 200)
+    return res
+  
+  res = make_response(jsonify({"error": "Collection not found"}), 400)
+  return res
+
 if __name__ == "__main__":
   print("Server runnning in port %s"%(PORT))
   app.run(host=HOST, port=PORT)
